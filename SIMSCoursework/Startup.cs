9bdbade50de63e7c59101593f05816f8e3b9d35c
victorbaxter <koolkido1412@gmail.com>
2018-02-12 16:32:44 +0700

    New Project

---
+++ SIMSCoursework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIMSCoursework.Startup))]
+namespace SIMSCoursework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
